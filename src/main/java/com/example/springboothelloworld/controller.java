5242e1e070433b33f84ae02bb99edb8afc41ba8b
MINGHONG <1848836348@qq.com>
2020-06-20 21:33:06 +0800

    带家试试下载到自己机子看看能不能运行

---
+++ src/main/java/com/example/springboothelloworld/controller.java
@@ -0,0 +1,10 @@
+package com.example.springboothelloworld;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class controller {
+    @GetMapping("hello")
+    public String sayHello(){
+        return "hello world123";
+    }
+}
